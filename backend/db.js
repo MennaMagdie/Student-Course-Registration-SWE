bfd7bd7bfd1ce42fb70db1513defbe1c261c555e
MennaMagdie <mennamagdyx@gmail.com>
2026-05-12 15:45:12 +0300

    working now

--- backend/db.js
+++ backend/db.js
@@ -5,7 +5,7 @@ const mysql = require('mysql2');
 const pool = mysql.createPool({
   host: 'localhost',
   user: 'root',
-  password: '', // Leave empty if no password
+  password: 'samalovesmenna', // Leave empty if no password
   database: 'university_db',
   waitForConnections: true,
   connectionLimit: 10,
